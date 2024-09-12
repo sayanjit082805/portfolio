55be8a35a72fe8787ac7dbe3da77052bd59c31fd
sayanjit082805 <sayanjitukil2005@gmail.com>
2024-09-12 22:21:20 +0530

    add changes

---
+++ app/page.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Spotlight } from "@/components/ui/Spotlight";
+
+export default function Home() {
+  return (
+    <>
+      <div className="h-[40rem] w-full flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden min-h-screen">
+        <Spotlight
+          className="-top-40 left-0 md:left-60 md:-top-20"
+          fill="white"
+        />
+        <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
+          <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+            Learning the ways of the world, <br /> one step at a time.
+          </h1>
+          <p className="mt-6 font-normal text-xl text-neutral-300 max-w-lg text-center mx-auto">
+            Hello there, dear traveller! My name is Sayanjit and I consider
+            myself to be a somewhat proficient frontend developer.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      animation: {
+        spotlight: "spotlight 2s ease .75s 1 forwards",
+      },
+      keyframes: {
+        spotlight: {
+          "0%": {
+            opacity: 0,
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity: 1,
+            transform: "translate(-50%,-40%) scale(1)",
+          },
+        },
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+};
