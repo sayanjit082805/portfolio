9a79392b596dad5adb449ac412a229f19c2b4420
sayanjit082805 <sayanjitukil2005@gmail.com>
2024-09-14 22:54:15 +0530

    add more details and continue

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,14 @@
-"use client"
+"use client";
 import "./globals.css";
 import React from "react";
 import { Spotlight } from "@/components/ui/Spotlight";
 import { motion } from "framer-motion";
+import Tech from "@/components/tech";
 
 export default function Home() {
   return (
     <>
-      <div className="h-[40rem] w-full flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden min-h-screen">
+      <div className="h-[40rem] w-full flex items-center justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden min-h-screen">
         <Spotlight
           className="-top-40 left-0 md:left-60 md:-top-20"
           fill="white"
@@ -21,7 +22,7 @@ export default function Home() {
             duration: 0.8,
             ease: "easeInOut",
           }}
-          className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0"
+          className=" p-4 max-w-7xl  mx-auto relative z-10  w-full md:pt-0"
         >
           <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
             Learning the ways of the world, <br /> one step at a time.
@@ -30,6 +31,29 @@ export default function Home() {
             Hello there, dear traveller! My name is Sayanjit and I consider
             myself to be a somewhat proficient frontend developer.
           </p>
+        </motion.div>
+      </div>
+      <div className="md:h-[40rem] w-full flex md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative md:overflow-hidden h-full">
+        <div className="md:w-[40rem] md:h-40 relative w-full max-w-7xl h-[1550px]">
+          <h1 className="text-6xl md:text-6xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50 lg:leading-tight">
+            Tech Stack
+          </h1>
+          <div className="absolute inset-x-20 top-20 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
+          <div className="absolute inset-x-20 top-20 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
+          <div className="absolute inset-x-60 top-20 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
+          <div className="absolute inset-x-60 top-20 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
+        </div>
+        <motion.div
+          initial={{ opacity: 0.0, x: 60 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{
+            delay: 0.3,
+            duration: 0.8,  
+            ease: "easeInOut",                                  
+          }}
+          className="grid gap-3 grid-cols-1 md:grid-cols-[repeat(auto-fill, _minmax(250px, _1fr))] mt-20 absolute md:left-32 w-full px-4"
+        >
+          <Tech />
         </motion.div>
       </div>
     </>
---
+++ components/tech.jsx
@@ -0,0 +1,115 @@
+import Image from "next/image";
+
+export default function Tech() {
+  const skills = [
+    {
+      imgSrc: "/skills/javascript.svg",
+      label: "JavaScript",
+      desc: "console.log('Hello, World!')",
+    },
+    {
+      imgSrc: "/skills/python.svg",
+      label: "Python",
+      desc: "print('Hello, World!')",
+    },
+    {
+      imgSrc: "/skills/lua.svg",
+      label: "Lua",
+      desc: "print('Hello, World!')",
+    },
+    {
+      imgSrc: "/skills/cpp.svg",
+      label: "C++",
+      desc: "cout << 'Hello, World!'",
+    },
+    {
+      imgSrc: "/skills/go.svg",
+      label: "Go",
+      desc: "fmt.Println('Hello, World!')",
+    },
+    {
+      imgSrc: "/skills/bash.svg",
+      label: "Shell Script",
+      desc: "echo 'Hello, World!'",
+    },
+    {
+      imgSrc: "/skills/java.svg",
+      label: "Java",
+      desc: "System.out.println('Hello, World!')",
+    },
+    {
+      imgSrc: "/skills/react.svg",
+      label: "React",
+      desc: "Just React",
+    },
+
+    {
+      imgSrc: "/skills/next.svg",
+      label: "Next.js",
+      desc: "Supercharged React",
+    },
+    {
+      imgSrc: "/skills/tailwindcss.svg",
+      label: "TailwindCSS",
+      desc: "Utility-First CSS",
+    },
+    {
+      imgSrc: "/skills/mui.svg",
+      label: "Material-UI",
+      desc: "UI Framework",
+    },
+    {
+      imgSrc: "/skills/vite.svg",
+      label: "Vite",
+      desc: "Blazing Fast Build Tool",
+    },
+    {
+      imgSrc: "/skills/vercel.svg",
+      label: "Vercel",
+      desc: "Serverless Deployment",
+    },
+    {
+      imgSrc: "/skills/firebase.svg",
+      label: "Firebase",
+      desc: "Cloud Services",
+    },
+    {
+      imgSrc: "/skills/postgres.svg",
+      label: "PostgreSQL",
+      desc: "Relational Database",
+    },
+    {
+      imgSrc: "/skills/framer.svg",
+      label: "Framer Motion",
+      desc: "Animation Library",
+    },
+  ];
+
+  return (
+    <>
+      <div className="flex flex-wrap justify-start gap-4 mt-10 ml-5 md:ml-0">
+        {skills.map((skill, index) => (
+          <div
+            key={index}
+            className="flex items-center gap-3 ring-2 ring-inset ring-zinc-50/10 rounded-2xl p-3 hover:bg bg-black/[0.96] antialiased transition-color hover:bg-zinc-800 w-11/12 md:w-96"
+          >
+            <figure className="hover:bg-neutral-900/[0.96] rounded-lg p-2 overflow-hidden w-12 h-12">
+              <Image
+                src={skill.imgSrc}
+                alt={skill.label}
+                width={48}
+                height={48}
+              />
+            </figure>
+            <div className="">
+              <h3 className="text-lg font-semibold text-white">
+                {skill.label}
+              </h3>
+              <p className="text-sm text-gray-400">{skill.desc}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
